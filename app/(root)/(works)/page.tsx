521a22c2bba50bef3f4a84533d8f7c1974bc5e71
HereHaveTheseFlowers <herehavetheseflowers@gmail.com>
2024-11-28 13:39:44 +0300

    adds home page

--- app/(root)/(works)/page.tsx
+++ app/(root)/(works)/page.tsx
@@ -1,9 +1,29 @@
 import Navbar from "@/components/Navbar/Navbar";
+import TextList from "@/components/TextList/TextList";
+import imageAny from "@/public/anybrowser.png";
+import imageChess from "@/public/chess.png";
+import "./styles.css";
 
 export default async function HomePage() {
   return (
     <>
-      <Navbar pagePath="works" />
+      <Navbar pagePath="/works" />
+      <TextList>
+        <TextList.Field>
+          <img
+            className="works__img"
+            src={imageAny.src}
+            alt="a screenshot of anyobjects.ru"
+          />
+        </TextList.Field>
+        <TextList.Field>
+          <img
+            className="works__img"
+            src={imageChess.src}
+            alt="a screenshot of anyobjects.ru"
+          />
+        </TextList.Field>
+      </TextList>
     </>
   );
 }
